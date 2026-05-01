b347a98724b239a86164bb1f3248f1ab9d0e37fc
SchEAx <enesagar.s3n3@gmail.com>
2026-05-01 21:54:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "garage-pwa-v4";
+const CACHE_NAME = "garage-pwa-v5";
 const urlsToCache = ["/", "/index.html", "/manifest.json", "/logo.png"];
 
 self.addEventListener("install", (event) => {
