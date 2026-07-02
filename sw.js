6265742b13852a638b1b315331da39b28def430e
SchEAx <enesagar.s3n3@gmail.com>
2026-07-02 11:32:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "garage-pwa-v11-bekleme-takip";
+const CACHE_NAME = "garage-pwa-v12-bekleme-workflow";
 const urlsToCache = ["/", "/index.html", "/manifest.json", "/logo.png"];
 
 self.addEventListener("install", (event) => {
